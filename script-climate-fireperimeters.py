8a5b688231cd55ffc796056c4ab41f605621d40c
dexmcmillan <dexmcmillan@gmail.com>
2022-06-09 13:51:39 -0400

    canada wildfire map

--- script-climate-fireperimeters.py
+++ script-climate-fireperimeters.py
@@ -1,47 +1,66 @@
-import requests
-import geopandas
 import pandas as pd
-import json
 import datawrappergraphics
-import logging
 import datetime as dt
 
+def strfdelta(tdelta, fmt):
+    d = {"days": tdelta.days}
+    d["hours"], rem = divmod(tdelta.seconds, 3600)
+    d["minutes"], d["seconds"] = divmod(rem, 60)
+    return fmt.format(**d)
+
 # Live chart ID: Os1m6
-MAP_ID = "80lJz"
-CHART_ID = "0NmJU"
+MAP_ID = "NyqLI"
 
 # Make HTTP request for the data and put into a geopandas dataframe.
-raw = geopandas.read_file("/vsicurl/https://cwfis.cfs.nrcan.gc.ca/downloads/hotspots/", driver="shapefile", layer="perimeters")
-# data = geopandas.read_file(json.dumps(r.json()))
-raw["type"] = "area"
-raw["LASTDATE"] = pd.to_datetime(raw["LASTDATE"])
-raw.loc[raw["LASTDATE"] == dt.datetime.today(), "active"] = True
-raw["active"] = raw["active"].fillna(False)
-print(raw)
+raw = pd.read_csv("https://cwfis.cfs.nrcan.gc.ca/downloads/activefires/activefires.csv")
 
-datawrappergraphics.Chart(MAP_ID).data(raw).head(f"There are {len(raw)} fires burning in Canada right now, covering {'{:,}'.format(raw['AREA'].sum())} hectares of land.")
+data = raw.loc[raw["agency"] != "ak", :]
+data.columns = data.columns.str.strip()
 
-# # Change colour of the marker based on the fire status.
-# data["markerColor"] = data["FIRE_STATUS"].replace({"Under Control": "#436170", "New": "#F8C325", "Out of Control": "#c42127", "Being Held": "#000000"})
+data["stage_of_control"] = data["stage_of_control"].str.strip().replace({
+    "OC": "Out of control",
+    "UC": "Under control",
+    "EX": "Extinguished",
+    "BH": "Being held"
+})
 
-# # Define type as point and icon as fire.
-# data["type"] = "point"
-# data["icon"] = "fire"
+data["province/territory"] = data["agency"].replace({
+    "sk": "Saskatchewan",
+    "ab": "Alberta",
+    "bc": "British Columbia",
+    "on": "Ontario",
+    "ns": "Nova Scotia",
+    "yt": "Yukon",
+    "nt": "Northwest Territories",
+    "qc": "Quebec",
+    "mn": "Manitoba",
+    "nl": "Newfoundland and Labrador",
+    "pe": "Prince Edward Island"
+})
 
-# # Calculate scale of each item based on the side of the fire.
-# avg = data["CURRENT_SIZE"].min()
-# std = data["CURRENT_SIZE"].std()
-# data["scale"] = ((data["CURRENT_SIZE"] - avg) / (std)) + 1
+data = data.rename(columns={
+    "lat": "Lat",
+    "lon": "Lon"
+})
+data["startdate"] = pd.to_datetime(data["startdate"])
 
-# # This line caps scale at a certain value.
-# data["scale"] = data["scale"].apply(lambda x: 2.2 if x > 2.2 else x)
+data["duration"] = dt.datetime.today() - data["startdate"]
 
-# # Build tooltip from description and status.
-# data["tooltip"] = "<big>Fire at " + data['GEOGRAPHIC_DESCRIPTION'] + "</big><br><b>Status</b>: " + data['FIRE_STATUS'] + "</span><br><b>Started by</b>: " + data["FIRE_CAUSE"] + "<br><b>Estimated size</b>: " + data["CURRENT_SIZE"].astype(str) + " hectares"
-# data = data.sort_values("scale")
+data = data.sort_values("duration", ascending=False)
 
-# map = (datawrappergraphics.Map(chart_id=MAP_ID)
-#             .data(raw)
-#             .head(f"TEST: Floods in Canada")
-#             .publish()
-#             )
+data["duration"] = data["duration"].apply(lambda x: strfdelta(x, "{days} days and {hours} hours"))
+
+longest_duration = data.at[0, "duration"]
+
+num_fires = len(data)
+percent_oc = int(len(data[data["stage_of_control"] == "Out of control"]) / len(data)*100)
+total_area = int(data["hectares"].sum())
+
+print(data)
+
+(datawrappergraphics.Chart(MAP_ID)
+ .data(data)
+ .head(f"There are <b>{num_fires:,} active wildfires</b> burning in Canada right now, covering {total_area:,} hectares of land.")
+ .deck(f"The longest has been burning for <b>{longest_duration}</b>. <b>{percent_oc}%</b> are classifed as out of control.<br><br>Size of the circles indicate relative size of the fire. Zoom in to see fires that are closely grouped.")
+ .publish()
+ )
