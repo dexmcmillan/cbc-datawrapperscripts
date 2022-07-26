9eaa37122e3d4db0733e66d9e16c48b9e7916a30
dexmcmillan <dexmcmillan@gmail.com>
2022-07-26 13:22:58 -0400

    small changes to heat warning map

--- script-climate-heatwarnings.py
+++ script-climate-heatwarnings.py
@@ -6,15 +6,6 @@ data = raw[raw["alert_type"] == "warning"]
 data["title"] = data["descrip_en"]
 data["fill-opacity"] = 0.8
 
-# print(data)
-
-# provinces = datawrappergraphics.Map("1XHZ5").get_markers()[-13:]
-# import json
-# with open('./assets/shapes/shapes-heatwarnings.json', 'w') as f:
-#     json.dump(provinces, f)
-
-# data = pd.concat()
-
 (datawrappergraphics.Map("1XHZ5")
  .data(data, append="./assets/shapes/shapes-heatwarnings.json")
  .head(f"There are {int(len(data))} heat warnings in effect across Canada right now")
