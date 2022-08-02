91989f8210020b20525dc2d9b215563d246de63a
dexmcmillan <dexmcmillan@gmail.com>
2022-08-02 09:07:48 -0400

    last update to keremeos map

--- script-climate-bcfiremap.py
+++ script-climate-bcfiremap.py
@@ -13,13 +13,14 @@ data = geopandas.read_file(json.dumps(r.json()))
 data = data.to_crs("EPSG:4326")
 
 data["stroke"] = "#C42127"
+data["stroke-width"] = 2
 data["fill"] = False
 
 evac = requests.get("https://services6.arcgis.com/ubm4tcTYICKBpist/ArcGIS/rest/services/Evacuation_Orders_and_Alerts/FeatureServer/0/query?where=ORDER_ALERT_STATUS+%3C%3E+%27All+Clear%27+AND+EVENT_TYPE+%3D+%27Fire%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&relationParam=&returnGeodetic=false&outFields=OBJECTID,EVENT_NAME,ORDER_ALERT_STATUS&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&defaultSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=")
 evac = geopandas.read_file(json.dumps(evac.json()))
 
 evac.loc[evac["ORDER_ALERT_STATUS"] == "Alert", "fill"] = "#F8C325"
-evac.loc[evac["ORDER_ALERT_STATUS"] == "Order", "fill"] = "#E06618"
+evac.loc[evac["ORDER_ALERT_STATUS"] == "Order", "fill"] = "#C42127"
 evac["stroke"] = False
 evac["title"] = evac["EVENT_NAME"]
 evac = evac[evac["title"] == "Keremeos Creek Wildfire"]
