3dc6760558afac2a5d4c9edf48bf9e3ca18f6784
dexmcmillan <dexmcmillan@gmail.com>
2022-08-02 09:29:56 -0400

    bcfiremap changes (again)

--- script-climate-bcfiremap.py
+++ script-climate-bcfiremap.py
@@ -16,27 +16,27 @@ data["stroke"] = "#C42127"
 data["stroke-width"] = 2
 data["fill"] = False
 
-evac = requests.get("https://services6.arcgis.com/ubm4tcTYICKBpist/ArcGIS/rest/services/Evacuation_Orders_and_Alerts/FeatureServer/0/query?where=ORDER_ALERT_STATUS+%3C%3E+%27All+Clear%27+AND+EVENT_TYPE+%3D+%27Fire%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&relationParam=&returnGeodetic=false&outFields=OBJECTID,EVENT_NAME,ORDER_ALERT_STATUS&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&defaultSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=")
-evac = geopandas.read_file(json.dumps(evac.json()))
+# evac = requests.get("https://services6.arcgis.com/ubm4tcTYICKBpist/ArcGIS/rest/services/Evacuation_Orders_and_Alerts/FeatureServer/0/query?where=ORDER_ALERT_STATUS+%3C%3E+%27All+Clear%27+AND+EVENT_TYPE+%3D+%27Fire%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&relationParam=&returnGeodetic=false&outFields=OBJECTID,EVENT_NAME,ORDER_ALERT_STATUS&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&defaultSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=")
+# evac = geopandas.read_file(json.dumps(evac.json()))
 
-evac.loc[evac["ORDER_ALERT_STATUS"] == "Alert", "fill"] = "#F8C325"
-evac.loc[evac["ORDER_ALERT_STATUS"] == "Order", "fill"] = "#C42127"
-evac["stroke"] = False
-evac["title"] = evac["EVENT_NAME"]
-evac = evac[evac["title"] == "Keremeos Creek Wildfire"]
+# evac.loc[evac["ORDER_ALERT_STATUS"] == "Alert", "fill"] = "#F8C325"
+# evac.loc[evac["ORDER_ALERT_STATUS"] == "Order", "fill"] = "#C42127"
+# evac["stroke"] = False
+# evac["title"] = evac["EVENT_NAME"]
+# evac = evac[evac["title"] == "Keremeos Creek Wildfire"]
 
-data = data.append(evac)
+# data = data.append(evac)
 
-# markers1 = datawrappergraphics.Map(chart_id=CHART_ID).get_markers()[0:2]
-# markers2 = datawrappergraphics.Map(chart_id=CHART_ID).get_markers()[-2:]
+markers1 = datawrappergraphics.Map(chart_id=CHART_ID).get_markers()[0:2]
+markers2 = datawrappergraphics.Map(chart_id=CHART_ID).get_markers()[-5:]
 
-# markers = [x for n in (markers1, markers2) for x in n]
+markers = [x for n in (markers1, markers2) for x in n]
 
 
-# with open('./assets/shapes/shapes-bcfiremap.json', 'w', encoding='utf-8') as f:
-#     json.dump(markers, f)
+with open('./assets/shapes/shapes-bcfiremap.json', 'w', encoding='utf-8') as f:
+    json.dump(markers, f)
 
-data["fill"] = data["fill"].fillna("#C42127")
+# data["fill"] = data["fill"].fillna("#C42127")
 
 map = (datawrappergraphics.Map(chart_id=CHART_ID)
             .data(data, append="./assets/shapes/shapes-bcfiremap.json")
